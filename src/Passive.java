dd420761bf33edaafe88481bd2f087d1ddf8c743
Gabriele Pittui <166812291+GabrieleProUNO@users.noreply.github.com>
2025-09-29 08:56:34 +0000

    Aggiungi la classe Passive e la classe Trinket estendendo Item con gestione dei modifiatori di danno e fuoco

---
+++ src/Passive.java
@@ -0,0 +1,7 @@
+package rogue;
+
+public class Passive extends Item {
+    public Passive(double damageMod, double fireMod) {
+        super(damageMod, fireMod);
+    }
+}
---
+++ src/Trinket.java
@@ -0,0 +1,12 @@
+package rogue;
+
+public class Trinket extends Item implements Collectible {
+    public Trinket(double damageMod, double fireMod) {
+        super(damageMod, fireMod);
+    }
+
+    @Override
+    public boolean canDropped() {
+        return true;
+    }
+}
